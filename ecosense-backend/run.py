b347548152281f0d5c917c0b7e4b389dd3edd79a
yudhistirank <yudhistiranandakumala@gmail.com>
2024-11-30 19:37:24 +0700

    update port + run.py + add dockerfile

--- ecosense-backend/run.py
+++ ecosense-backend/run.py
@@ -6,5 +6,5 @@ load_dotenv()
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 3000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.getenv('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
